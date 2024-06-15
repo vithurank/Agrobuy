158ea3f4f0c16b96ece55697c0bf43ee3a902a39
Vithuran K <vithuran0311@gmail.com>
2024-06-15 15:07:38 +0530

    navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+     
     return (
         <div className='navbar'>
             <div className='nav-logo'>
@@ -10,14 +13,15 @@ const Navbar = () => {
                 <p>Agrobuy</p>
             </div>
             <ul className='nav-menu'>
-                <li>Shop</li>
-                <li>Equipments</li>
-                <li>Seeds</li>
-                <li>Fertilizers</li>
+                <li onClick={()=>{setMenu("shop")}}>Shop<hr/></li>
+                <li onClick={()=>{setMenu("equipments")}}>Equipments</li>
+                <li onClick={()=>{setMenu("seeds")}}>Seeds</li>
+                <li onClick={()=>{setMenu("fertilizers")}}>Fertilizers</li>
             </ul>
             <div className='nav-login-cart'>
                 <button>Login</button>
                 <img src={cart_icon} alt='cart_icon'/>
+                <div className='nav-cart-count'>0</div>
             </div>
         </div>
     );
