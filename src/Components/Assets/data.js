65166350f737ba431d2f66381b766a04e2b7cdbb
Vithuran K <vithuran0311@gmail.com>
2024-06-30 20:36:50 +0530

    hero_and_popular

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,3 +1,4 @@
+
 import p1_img from './product_1.png'
 import p2_img from './product_2.png'
 import p3_img from './product_3.png'
@@ -6,28 +7,28 @@ import p4_img from './product_4.png'
 let data_product = [
   {
     id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Heirloom Tomato Seeds",
     image:p1_img,
-    new_price:50.00,
-    old_price:80.50,
+    new_price:199.00,
+    old_price:250.00,
   },
   {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Premium Watermelon Seeds",
     image:p2_img,
-    new_price:85.00,
-    old_price:120.50,
+    new_price:299.00,
+    old_price:400.00,
   },
   {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Aromatic Mint Seeds",
     image:p3_img,
     new_price:60.00,
-    old_price:100.50,
+    old_price:450.00,
   },
   {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Vibrant Sunflower Seeds",
     image:p4_img,
-    new_price:100.00,
-    old_price:150.00,
+    new_price:113.00,
+    old_price:250.00,
   },
 ];
 
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,21 +8,22 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className="hero-left">
-                <h2>NEW ARRIVALS ONLY</h2>
-                <div className="hand-hand-icon">
-                    <p>New</p>
-                    <img src={hand_icon} alt="hand_icon"/>
+                <h2>Empowering Your Farm, One Tool at a Time</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p>new</p>
+                        <img src={hand_icon} alt="hand_icon"/>
+                    </div>
+                    <p>collection</p>
+                    <p>for farmers</p>
                 </div>
-                <p>collection</p>
-                <p>for everyone</p>
-            </div>
-            <div className="hero-latest-btn">
-                <div>Latest Collection</div>
-                <img src={arrow_icon} alt="arrow_icon"/>
+                <div className="hero-latest-btn">
+                    <div>Latest collection</div>
+                    <img src={arrow_icon} alt="arrow_icon"/>
+                </div>
             </div>
             <div className="hero-right">
-                <img src={hero_image} alt="hero_image"/>
-
+                <img src={hero_image} alt="hero_image" height={800}/>
             </div>
         </div>
     );
