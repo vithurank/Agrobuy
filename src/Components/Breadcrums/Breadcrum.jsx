0c45f21b9e696b0ce16e3c4ed7d98bbcab4471a5
Vithuran K <vithuran0311@gmail.com>
2024-08-28 11:34:09 +0530

    ProductDisplay

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Breadcrum.css';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
+
+const Breadcrums = (props ) => {
+    const {product} = props;
+    return (
+        <div className='breadcrum'>
+            Home <img src={arrow_icon} alt='arrow_icon' /> SHOP <img src={arrow_icon} alt='arrow_icon' /> {product.category} <img src={arrow_icon} alt='arrow_icon' /> {product.name}
+        </div>
+    );
+};
+
+export default Breadcrums;
---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './ProductDisplay.css';
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+    return (
+        <div className='productdisplay'>
+            <div className='productdisplay-left'>
+                <div className='productdisplay-img-list'>
+                    <img src ={product.image} alt='product'/>
+                    <img src ={product.image} alt='product'/>
+                    <img src ={product.image} alt='product'/>
+                    <img src ={product.image} alt='product'/>
+                </div>
+                <div className='productdisplay-img'>
+                    <img className='productdisplay-img-main' src ={product.image} alt='product'/>
+                </div>
+            </div>
+            <div className='productdisplay-right'>
+                <h1>{product.name}</h1>
+                <div className='productdisplay-right-star'>
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_dull_icon} alt="" />
+                    <p>(122)</p>
+                </div>
+                <div className='productdisplay-right-prices'>
+                    <div className='productdisplay-right-price-old'>Rs.{product.old_price}</div>
+                    <div className='productdisplay-right-price-new'>Rs.{product.new_price}</div>
+                </div>
+                <div className='productdisplay-right-description'>
+                Discover a world of growth and productivity at AgroBuy! We specialize in providing top-quality agricultural equipment, premium seeds, and nutrient-rich fertilizers. Whether you’re a seasoned farmer or a budding gardener, AgroBuy has everything you need to nurture your land and reap bountiful harvests.
+                </div>
+                <button> ADD TO CART </button>
+                <p className='productdispaly-right-category'><span>Category :</span> Equipment, Machine, Tractor</p>
+                <p className='productdispaly-right-category'><span>Tags :</span> Equipment, Modern, Latest</p>
+            </div>
+        </div>
+    );
+}; 
+
+
+export default ProductDisplay; 
